62743a7fb979c386aaa89ccb152d7d059c74e6d3
Ayush Solanke <ayushsolanke47@gmail.com>
2026-08-02 12:38:27 +0530

    check upstream in new_branch

--- message.js
+++ message.js
@@ -1,3 +1,4 @@
 console.log('Hii My Friends, i am learning Git')
 console.log('Here I create a branch and push it');
 console.log('Checking upstream in this branch also');
+console.log('Checking upstream in this new_branch');
